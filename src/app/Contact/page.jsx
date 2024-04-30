68c4cd660f01b39b99ef810a9dbda49c3fa8cad4
sssssaharat <saharatseerakoon@gmail.com>
2024-04-30 21:33:46 +0700

    Contact

--- src/app/Contact/page.jsx
+++ src/app/Contact/page.jsx
@@ -1,12 +1,26 @@
-import React from 'react'
+import React from "react";
 
-function Contact
-() {
+function Contact() {
   return (
-    <div>Contact
-        
+    <div className="mx-auto max-w-7xl py-32 sm:py-48 lg:py-28">
+      <div className="bg-gradient-to-br from-white to-gray-400 bg-clip-text text-center text-7xl sm:text-[120px] md:text-[150px] lg:text-[200px]  font-medium leading-tight text-transparent sm:leading-tight  md:leading-tight">
+        Ready to create?
+      </div>
+      <div className="mx-auto max-w-4xl py-32 sm:py-48 lg:py-28">
+        <div className="flex flex-col mx-12">
+          <p className="text-indigo-500 text-md py-4">Contact details</p>
+          <p className="text-white text-2xl pb-3">sssssaharat.s@gmail.com</p>
+          <p className="text-white text-2xl">0804604129</p>
+        </div>
+        <div className="flex flex-col mx-12 py-32">
+          <p className="text-indigo-500 text-md py-4">Address</p>
+          <p className="text-white text-2xl pb-3">59/42 Lasalle 71 </p>
+          <p className="text-white text-2xl pb-3">Bangna Street</p>
+          <p className="text-white text-2xl">Bangkok 10260.</p>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Contact
+export default Contact;
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -9,16 +9,14 @@ export default function Home() {
     setWidth(newWidth);
   };
   return (
-    <div className="relative mx-auto max-w-7xl isolate px-6 ">
+    <div className="relative mx-auto max-w-7xl isolate  ">
       <div className="mx-auto max-w-7xl py-32 sm:py-48 lg:py-6">
         <section className="Title">
           <div className="relative items-center ">
-            <div className="relative flex items-center justify-center">
-              <div className="absolute bg-gradient-to-br from-white to-gray-400 bg-clip-text text-center text-9xl xs:text-7xl sm:text-[160px] md:text-[180px] lg:text-[200px] font-medium leading-tight text-transparent sm:leading-tight  md:leading-tight">
+            <div className="flex items-center justify-center">
+              <div className="absolute bg-gradient-to-br from-white to-gray-400 bg-clip-text text-center text-7xl sm:text-[150px] md:text-[200px] lg:text-[250px]  font-medium leading-tight text-transparent sm:leading-tight  md:leading-tight">
                 Portfolio
-                <div className="text-8xl xs:text-9xl sm:text-7xl md:text-[130px] lg:text-[170px] ">
-                  sssssaharat
-                </div>
+                <div className="text-6xl  sm:text-[100px] ">sssssaharat</div>
               </div>
 
               <Image
@@ -33,10 +31,11 @@ export default function Home() {
         </section>
         <section className="present">
           <div className="mx-auto max-w-2xl text-center sm:text-center py-32 sm:py-48 lg:py-64">
-            <p className="mt-6 text-2xl text-gray-100 font-light sm:text-3xl leading-tight text-transparent sm:leading-tight  md:leading-tight">
-              Interested in{" "}
-              <span className="text-indigo-500">Fontent, UX & UI designer</span>{" "}
-              focussing on designing own<br></br>Trade dashboard & website
+            <p className="mt-6 text-2xl text-gray-100 font-light sm:text-4xl leading-tight  sm:leading-tight  md:leading-tight">
+              Interested in
+              <span className="text-indigo-500"> Fontent,UX & UI </span>
+              <br></br>
+              focussing on designing own dashboard Trade and website Tecnical
               Trade.
             </p>
           </div>
